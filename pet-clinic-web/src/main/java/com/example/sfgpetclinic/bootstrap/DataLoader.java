c1322505ce8a7c318f24f8d3bd4a569d9cc40cb6
Mario <mario.butir@fer.hr>
2018-08-05 16:56:24 +0200

    added pets and contact info on startup

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package com.example.sfgpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.model.Pet;
 import com.example.sfgpetclinic.model.PetType;
 import com.example.sfgpetclinic.model.Vet;
 import com.example.sfgpetclinic.services.OwnerService;
@@ -28,21 +31,41 @@ public class DataLoader implements CommandLineRunner {
 		
 		PetType dog = new PetType();
 		dog.setName("Doge");
-		PetType savedDogPetTyoe = petTypeService.save(dog);
+		PetType savedDogPetType = petTypeService.save(dog);
 		
 		PetType cat = new PetType();
 		dog.setName("Cate");
-		PetType savedCatPetTyoe = petTypeService.save(cat);
+		PetType savedCatPetType = petTypeService.save(cat);
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Milan");
 		owner1.setLastName("Mileni");
+		owner1.setAddress("Ilica 123");
+		owner1.setCity("Zagreb");
+		owner1.setTelephone("092212212");
+		
+		Pet milanovPet = new Pet();
+		milanovPet.setPetType(savedDogPetType);
+		milanovPet.setName("Frane");
+		milanovPet.setBirthDate(LocalDate.now());
+		milanovPet.setOwner(owner1);
+		owner1.getPets().add(milanovPet);
 
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Luka");
 		owner2.setLastName("Lekić");
+		owner1.setAddress("Ilica 213");
+		owner1.setCity("Zagreb");
+		owner1.setTelephone("092131231");
+		
+		Pet lukinPet = new Pet();
+		lukinPet.setPetType(savedCatPetType);
+		lukinPet.setName("Mate");
+		lukinPet.setBirthDate(LocalDate.now());
+		lukinPet.setOwner(owner2);
+		owner2.getPets().add(lukinPet);
 
 		ownerService.save(owner2);
 		System.out.println("Loading owners...");
