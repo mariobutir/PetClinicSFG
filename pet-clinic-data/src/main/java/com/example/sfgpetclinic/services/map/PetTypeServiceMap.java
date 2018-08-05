72376bd9c2732ddac5078ec9045d11a2d953a433
Mario <mario.butir@fer.hr>
2018-08-05 16:22:27 +0200

    added pet type service map and pet type bootstrap

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,38 @@
+package com.example.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+
+import com.example.sfgpetclinic.model.PetType;
+import com.example.sfgpetclinic.services.PetTypeService;
+
+@Service
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+}
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -4,8 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.model.PetType;
 import com.example.sfgpetclinic.model.Vet;
 import com.example.sfgpetclinic.services.OwnerService;
+import com.example.sfgpetclinic.services.PetTypeService;
 import com.example.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,14 +15,24 @@ public class DataLoader implements CommandLineRunner {
 
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		PetType dog = new PetType();
+		dog.setName("Doge");
+		PetType savedDogPetTyoe = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		dog.setName("Cate");
+		PetType savedCatPetTyoe = petTypeService.save(cat);
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Milan");
